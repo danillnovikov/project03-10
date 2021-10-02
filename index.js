2f9c8feb84700b74482b7c48ce2e8f27770c9390
Daniil <danill.novikov@mail.ru>
2021-10-03 01:04:11 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+//Массив 
+
+let arr = [];
+//let sum = 0;
+
+
+function sumInput() {
+
+  for(i = 1; i < 3; i++) {
+  arr.push(+prompt('Введите число',));
+  }
+  return console.log(sum+=arr);
+  
+}
+
+sumInput();
+
+
+
+
+// function sumInput(){ 
+//   arr.push(a);
+//    return console.log(arr);
+
+// }
+
+// sumInput();
